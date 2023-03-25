2e419d3de9856d2996fbdde4216b0697de2a49c6
Yash Goyal <yashgo0018@gmail.com>
2023-03-25 09:29:10 +0530

    added hardhat config and etherscan verification

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { ethers } from "hardhat";
+import { ethers, run } from "hardhat";
 
 const royaltyBasisPoints = 50;
 const oneOffFees = 100;
@@ -15,10 +15,18 @@ async function main() {
   );
 
   const organizationController = await OrganizationControllerFactory.deploy();
-  await organizationController.deployed();
+  await organizationController.deployTransaction.wait(10);
   console.log(
     `The organizaiton controller is deployed to ${organizationController.address}`
   );
+  try {
+    await run("verify:verify", {
+      address: organizationController.address,
+      constructorArguments: [],
+    });
+  } catch (err) {
+    console.log(err);
+  }
 
   const dealController = await DealControllerFactory.deploy(
     organizationController.address,
@@ -26,8 +34,35 @@ async function main() {
     oneOffFees,
     royaltyBasisPoints
   );
-  await dealController.deployed();
+  await dealController.deployTransaction.wait(10);
   console.log(`The deal controller is deployed to ${dealController.address}`);
+
+  try {
+    await run("verify:verify", {
+      address: dealController.address,
+      constructorArguments: [
+        organizationController.address,
+        account,
+        oneOffFees,
+        royaltyBasisPoints,
+      ],
+    });
+  } catch (err) {
+    console.log(err);
+  }
+
+  const logger = await organizationController.logger();
+  console.log(logger);
+
+  await organizationController.grantRole(
+    await organizationController.DEAL_MAKER_ROLE(),
+    dealController.address
+  );
+
+  await dealController.grantRole(
+    await dealController.SIGNER_ROLE(),
+    "0xC3A877573CC5844D2E9E514e3a187068FefA0cA0"
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
